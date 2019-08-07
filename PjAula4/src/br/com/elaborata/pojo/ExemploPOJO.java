c3dcbbbb3f4f76a25e7010d40ea338f8eaf1f9d3
ladp17 <leonardoadpereira@gmail.com>
2019-08-07 18:59:31 -0300

    testando o gitignore

---
+++ PjAula4/src/br/com/elaborata/pojo/ExemploPOJO.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package br.com.elaborata.pojo;
+
+/**
+ * @author Leo
+ *
+ */
+public class ExemploPOJO {
+
+	private String nome;
+	private String descricao;
+	private Integer idade;
+	private Double valor;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((idade == null) ? 0 : idade.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ExemploPOJO other = (ExemploPOJO) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (idade == null) {
+			if (other.idade != null)
+				return false;
+		} else if (!idade.equals(other.idade))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+	
+	
+
+}
